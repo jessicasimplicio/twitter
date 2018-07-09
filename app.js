e695fc40c1fb0b81b831b9bd5b1500027ec0697b
J. Simplicio <jessicasimplicio1@gmail.com>
2018-07-09 19:45:54 -0300

    versao 0.0.3: contador muda de cor e desativa botao

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ VERSÃO 0.0.1
 - Adicione um evento de click ao botão ou de submit no formulário. ok
 - No evento, obter o texto. --> salvar o texto numa variável ok
 - Adicione o texto ao HTML.
-criar um elemento <p> com o texto do input no .js
 */
 
 var container = document.getElementsByClassName("container")[0];
 document.getElementsByClassName("btn-tweet")[0].disabled = true;
-document.getElementById("counter").textContent = 140;
+var counter = document.getElementById("counter").textContent = 140;
 
 function clickButton() {
 	var text = document.getElementsByClassName("message")[0].value;
@@ -21,27 +20,47 @@ function clickButton() {
 }
 
 /* VERSÃO 0.0.2
--Não inserir texto vazio (desativar o botão "twittar").
-	comecar botao desabilitado:
-	document.getElementById("btn-tweet").disabled = true;
-	quando comeca a digitar, habilita de volta
+-Não inserir texto vazio (desativar o botão "tweet")
 
+========> ARRUMAR
 -Conte o número de caracteres de forma regressiva.
-	QUando algo é pressionado (excessao de enter e espaço) uma variavel é decrementada
-
+	QUando algo é pressionado (excessao de enter) uma variavel é decrementada
+	se for Backspace tem q incrementar
 */
 
 function enableButton(event) {
 	document.getElementsByClassName("btn-tweet")[0].disabled = false;
   var charCode = event.which || event.keyCode; //pega o keyCode do caracter
-  console.log(typeof(charCode));
-  console.log(charCode);
+  //console.log(typeof(charCode));
+  //console.log(charCode);
   if (charCode !== 32 || charCode !== 13){ counterDecrease(); } //nao ta funcionando
 }
 
 function counterDecrease() {
-	document.getElementById("counter").textContent -= 1;;
+	document.getElementById("counter").textContent -= 1;
+	counter -= 1;
+	console.log(typeof(counter));
+	if (counter < 0){
+		document.getElementsByClassName("btn-tweet")[0].disabled = true;
+	} 
+	if (counter <= 20 && counter > 10){
+		document.getElementById("counter").style.color = "orange";
+	}
+	if (counter <= 10){
+		document.getElementById("counter").style.color = "red";
+	}
 }
 
+function counterIncrease() { //para backspace
+
+}
+
+
+/*VERSÃO 0.0.3   ==> ARRUMAR BACKSPACE
+Se você passar os 140 caracteres, desative o botão. ok
+Se você passar os 120 caracteres, mostre o contador com outra cor. (counter entre 20 e 10) ok
+Se você passar os 130 caracteres, mostre o contador com outra cor. ok
+Se você passar os 140 caracteres, mostre o contador em negativo. ok
+*/
 
 
